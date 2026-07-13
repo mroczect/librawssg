94775d465d191ad6f8767f6d2f4e972d799adf29
mroczect <mroczect@proton.me>
2026-07-13 20:00:59 +0700

    refactor: simplify config loader, add DefaultConfig

--- src/config/loader.rs
+++ src/config/loader.rs
@@ -1,24 +1,30 @@
 use super::ConfigLoader;
 use crate::error::RawssgError;
-use crate::types::GlobalConfig;
+use crate::types::RawssgConfig;
 use std::path::Path;
 
-pub struct YamlConfigLoader<P: AsRef<Path>> {
+pub struct YamlConfigLoader<P: AsRef<Path> + Send + Sync> {
     path: P,
 }
 
-impl<P: AsRef<Path>> YamlConfigLoader<P> {
-    pub fn new(path: P) -> Self {
-        Self { path }
+impl<P: AsRef<Path> + Send + Sync> YamlConfigLoader<P> {
+    pub fn new(path: P) -> Self { Self { path } }
+}
+
+impl<P: AsRef<Path> + Send + Sync> ConfigLoader for YamlConfigLoader<P> {
+    #[tracing::instrument(skip(self))]
+    fn load(&self) -> Result<RawssgConfig, RawssgError> {
+        let content = std::fs::read_to_string(self.path.as_ref())
+            .map_err(|e| RawssgError::Config(format!("cannot read config: {}", e)))?;
+        serde_yaml::from_str(&content)
+            .map_err(|e| RawssgError::Config(format!("invalid config YAML: {}", e)))
     }
 }
 
-impl<P: AsRef<Path>> ConfigLoader for YamlConfigLoader<P> {
-    #[tracing::instrument(skip(self))]
-    fn load(&self) -> Result<GlobalConfig, RawssgError> {
-        let content = std::fs::read_to_string(self.path.as_ref())
-            .map_err(|e| RawssgError::Config(format!("cannot read config file: {}", e)))?;
-        serde_yaml::from_str(&content)
-            .map_err(|e| RawssgError::Config(format!("invalid config YAML: {}", e)))
+pub struct DefaultConfig;
+
+impl ConfigLoader for DefaultConfig {
+    fn load(&self) -> Result<RawssgConfig, RawssgError> {
+        Ok(RawssgConfig::default())
     }
 }
