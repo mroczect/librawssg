e87d633f1d082e75d561d10d0f64d60854135278
mroczect <mroczect@proton.me>
2026-07-26 23:07:02 +0700

    feat(librawssg): accept context builder in sitemap generation

--- src/site/sitemap.rs
+++ src/site/sitemap.rs
@@ -1,19 +1,21 @@
 #[cfg(feature = "tera")]
+use super::context::SitemapContextBuilder;
+#[cfg(feature = "tera")]
 use crate::error::RawssgError;
 #[cfg(feature = "tera")]
 use crate::site::TemplateRenderer;
 #[cfg(feature = "tera")]
-use crate::types::PageContext;
+use crate::types::{PageContext, RawssgConfig};
 
 #[cfg(feature = "tera")]
-#[tracing::instrument(skip(renderer))]
+#[tracing::instrument(skip(renderer, context_builder))]
 pub fn generate_sitemap(
     renderer: &dyn TemplateRenderer,
+    config: &RawssgConfig,
     pages: &[PageContext],
     base_url: &str,
+    context_builder: &dyn SitemapContextBuilder,
 ) -> Result<String, RawssgError> {
-    let mut ctx = tera::Context::new();
-    ctx.insert("pages", pages);
-    ctx.insert("base_url", base_url);
-    renderer.render("sitemap.xml", &ctx)
+    let ctx = context_builder.build_sitemap_context(config, pages, base_url)?;
+    renderer.render(&config.generators.sitemap.template, &*ctx)
 }
