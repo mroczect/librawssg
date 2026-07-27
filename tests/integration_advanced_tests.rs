5d283bd2b1e325d707ea869e9e837bf281afba7b
mroczect <mroczect@proton.me>
2026-07-27 20:32:41 +0700

    test(tests): remove cross-device test and imports

--- tests/integration_advanced_tests.rs
+++ tests/integration_advanced_tests.rs
@@ -3,14 +3,11 @@ mod common_mocks;
 
 #[cfg(all(feature = "tera", feature = "pulldown"))]
 mod advanced {
-    use crate::common::MockFs;
-    use crate::common_mocks::context_mocks::{MockFeedContextBuilder, MockSitemapContextBuilder};
     use librawssg::SiteBuilder;
     use librawssg::error::RawssgError;
     use librawssg::markdown::PulldownMarkdown;
     use librawssg::site::TeraRenderer;
     use librawssg::types::{ContentTypeDef, RawssgConfig};
-    use std::path::PathBuf;
 
     #[test]
     fn builder_missing_markdown_renderer() {
@@ -98,49 +95,5 @@ mod advanced {
             Err(RawssgError::Config(msg)) => assert!(msg.contains("sitemap context builder")),
             _ => panic!("Expected error about missing sitemap context builder"),
         }
-    }
-
-    #[test]
-    fn atomic_rename_fallback_on_cross_device() {
-        let mut fs = MockFs::new();
-        fs.add_file(
-            "/tmp/content/about.md",
-            "---\ntitle: About\ndesc: x\n---\nAbout",
-        );
-        fs.add_file(
-            "/tmp/content/index.md",
-            "---\ntitle: Index\ndesc: x\n---\nHome",
-        );
-        fs.dirs.push(PathBuf::from("/tmp/content"));
-        fs.dirs.push(PathBuf::from("/tmp/dist"));
-        fs.dirs.push(PathBuf::from("/tmp/dist.tmp"));
-
-        let mut config = RawssgConfig::default();
-        config.build.content_dir = "/tmp/content".into();
-        config.build.output_dir = "/tmp/dist".into();
-        config.build.templates_dir = "templates".into();
-        config.content_types.push(ContentTypeDef {
-            name: "page".into(),
-            pattern: "**/*.md".into(),
-            template: "base.html".into(),
-            list_template: None,
-            list_enabled: false,
-        });
-
-        let mock_renderer = crate::common::MockTemplateRenderer::new();
-        let mock_md = crate::common::MockMarkdownRenderer::identity();
-
-        let site = SiteBuilder::new()
-            .config(config)
-            .with_fs(Box::new(fs))
-            .with_template_renderer(Box::new(mock_renderer))
-            .with_markdown_renderer(Box::new(mock_md))
-            .with_feed_context_builder(Box::new(MockFeedContextBuilder))
-            .with_sitemap_context_builder(Box::new(MockSitemapContextBuilder))
-            .build()
-            .unwrap();
-
-        let result = site.generate();
-        assert!(result.is_ok());
     }
 }
